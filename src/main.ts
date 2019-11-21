29b6079772e699d92c010433e64545b9b8c35444
praburangki <praburangki@gmail.com>
2019-11-21 16:04:07 +0800

    use fastify adapter instead of the default one

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 const HOST_PORT = 3007;
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   await app.listen(HOST_PORT);
 
   Logger.log(`Listening at port: ${HOST_PORT}`);
