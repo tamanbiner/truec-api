1c00ac559fb95c357e57fb6e3aabfb053be7b883
praburangki <praburangki@gmail.com>
2019-11-22 15:59:30 +0800

    setup base user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import { IsEmail } from 'class-validator';
+import * as crypto from 'crypto';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  @IsEmail()
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @BeforeInsert()
+  hashPassword(): void {
+    this.password = crypto.createHmac('sha256', this.password).digest('hex');
+  }
+
+  @Column({ nullable: false })
+  fullName: string;
+}
